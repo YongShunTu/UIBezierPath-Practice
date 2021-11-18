4920b8d548c5d3e37744d7808d4f4a1e4d229d7e
YongShunTu <a49980060@gmail.com>
2021-11-19 02:16:22 +0800

    Initial Commit

---
+++ UIBezierPath Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIBezierPath Practice
+//
+//  Created by 姜又寧 on 2021/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIBezierPath Practice/UIBezierPath_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIBezierPath_PracticeApp.swift
+//  UIBezierPath Practice
+//
+//  Created by 姜又寧 on 2021/11/19.
+//
+
+import SwiftUI
+
+@main
+struct UIBezierPath_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
